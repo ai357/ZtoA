e853d455c7ddb9f3132fbd75622f02beca40336d
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2022-12-31 15:07:31 +0000

    routes更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,23 @@ Rails.application.routes.draw do
   devise_for :companies
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
+  namespace :company do
+  resources :schedules
+  resources :posts
+  resources :comments
+  root to: 'homes#top'
+  end
+  
+  
+  namespace :user do
+  resources :users
+  resources :posts
+  resources :comments
+  root to: 'homes#top'
+  end
+  
+  
+  
 end
