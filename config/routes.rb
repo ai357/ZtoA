271e88a52d4c4781a248f117163bf5fdf1666dd8
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2023-01-01 13:03:52 +0000

    devise-routes更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,34 @@
 Rails.application.routes.draw do
-  devise_for :companies
-  devise_for :users
+
+  #企業用
+  devise_for :companies, controllers: {
+    registrations: "company/registraitons",
+    sessions: 'company/sessions'
+  }
+
+  #利用者用
+  devise_for :users, controllers: {
+    registrations: "user/registraitons",
+    sessions: "user/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
-  
+
+
   namespace :company do
   resources :schedules
   resources :posts
   resources :comments
   root to: 'homes#top'
   end
-  
-  
+
+
   namespace :user do
   resources :users
   resources :posts
   resources :comments
   root to: 'homes#top'
   end
-  
-  
-  
+
+
+
 end
