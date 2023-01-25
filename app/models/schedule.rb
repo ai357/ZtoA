a0845b17dce0fa1af8fac1c35e9f74d370560129
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2023-01-25 15:24:55 +0000

    カレンダーにツールチップを追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,7 +2,7 @@ class Schedule < ApplicationRecord
 
 
 enum leave_stage: {
-  leave_day: 0, #aaa
+  leave_day: 0, 
   birth_day: 1,
   childcare_leave_1month: 2,
   childcare_leave_3month: 3,
@@ -12,6 +12,22 @@ enum leave_stage: {
   childcare_leave_11month: 7,
   childcare_leave_last: 8,  #ステータスが育児中に代わる
 }
+
+def self.tooltip(stage)
+  tooltip_word = {
+    "leave_day" => "産前休業",
+    "birth_day" => "産後休業",
+    "childcare_leave_1month" => "育児休業1ヵ月目",
+    "childcare_leave_3month" => "育児休業3ヵ月目",
+    "childcare_leave_5month" => "育児休業5ヵ月目",
+    "childcare_leave_7month" => "育児休業7ヵ月目",
+    "childcare_leave_9month" => "育児休業9ヵ月目",
+    "childcare_leave_11month" => "育児休業11ヵ月目",
+    "childcare_leave_last" => "育児中"
+  }
+
+  tooltip_word[stage]
+end
 
 end
 # == Schema Information
