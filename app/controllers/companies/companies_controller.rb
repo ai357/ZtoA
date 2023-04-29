bd6b6eac4ac61411576d6f170997e5d8e005325e
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2023-04-29 12:07:28 +0000

    companies/companyコメント

--- app/controllers/companies/companies_controller.rb
+++ app/controllers/companies/companies_controller.rb
@@ -2,24 +2,29 @@ class Companies::CompaniesController < ApplicationController
   before_action :authenticate_company!, except: %i[ unsubscribe withdraw ]
   
   def show
+    # 企業詳細（企業用）
     @company = current_company
   end
   
   def edit
+    # 企業編集（企業用）
     @company = current_company
   end
   
   def update
+    # 企業情報更新（企業用）
     @company = current_company
     @company.update(company_params)
     redirect_to companies_company_path
   end
 
   def unsubscribe
+    # 企業退会確認（企業用）
     @company = current_company
   end
 
   def withdraw
+    # 企業退会（企業用）
     @company = current_company
     @company.update(is_deleted: true)
     reset_session
